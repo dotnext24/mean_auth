e68526a0fbbe57469d6f54ba847edc75b7e78df3
dineshc <Dinesh.chaturvedi@goodmaysys.com>
2018-05-25 13:54:05 +0530

    dotnext24

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,8 @@ require('../config/passport')(passport);
 var express = require('express');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
-var User = require("../models/User.js");
-var Book = require("../models/Book.js");
+var User = require("../models/user");
+var Book = require("../models/book");
 
 /* GET home page. */
 router.get('/profile', function(req, res, next) {
